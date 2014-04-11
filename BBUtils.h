fc9a1cfde52002f6b637398dcae22f598a6f7ba9
bert <bert@multitask-media.com>
2014-04-11 14:59:22 -0400

    added utils file with print macros to disable Serial.print calls to make HairlessMIDI happy

---
+++ BBUtils.h
@@ -0,0 +1,13 @@
+#ifndef BBUtils_h
+#define BBUtils_h
+
+#define SUPPRESS_SERIAL_PRINT true
+
+#define printf(val) if(!SUPPRESS_SERIAL_PRINT) Serial.println(F(val))
+#define print(val) if(!SUPPRESS_SERIAL_PRINT) Serial.println(val)
+
+#endif // BBUtils_h
+
+
+
+
